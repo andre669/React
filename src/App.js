54d63294653c1d6a7e851ba7323063fcb5093387
Andre <andrefp2013@gmail.com>
2022-03-22 22:33:10 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './components/Menu';
+
+function App() {
+  return (
+    <div>
+      <Menu></Menu>
+    <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
